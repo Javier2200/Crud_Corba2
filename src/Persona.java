88bbc2398b6b2d9e3f8288a5bd9e41d269693cf8
JONEL <JONEL@JONEL-VAIO>
2018-04-25 21:12:03 -0500

    Creacion del PersonaFrm

--- src/Persona.java
+++ src/Persona.java
@@ -5,12 +5,23 @@ import org.omg.CORBA.ORB;
 
 public class Persona extends PersonaApp.PersonaPOA{
     private ORB orb;
+    Conexion conex = new Conexion();
+    
     @Override
     public boolean insertarPersona(int identificacion, String nombre, String apellido, String direccion, String telefono, String email) {
         boolean resultado = false;
         try {
             String query = "insert into persona(identificacion,nombre,apellido,direccion,telefono,email)"
                     + "values ("+identificacion+","+nombre+","+apellido+","+direccion+","+telefono+","+email+")";
+            conex.conexion();
+            Statement st = conex.conex.createStatement();
+            int valor = st.executeUpdate(query);
+            if (valor > 0){
+                resultado = true;
+            }
+            //Cerramos los recursos.
+            st.close();
+            conex.conex.close();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "ocurrio un error "+e.getMessage());
         }
